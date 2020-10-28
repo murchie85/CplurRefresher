7a6d1ec1d863ebb79d83d9258c63b8546257d2a8
murchie85 <murchie85@gmail.com>
2020-10-28 12:42:03 +0000

    first

---
+++ demo1/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: adammcmurchie
+ *
+ * Created on 28 October 2020, 12:38
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
